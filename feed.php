d876fa69385cf7c70955552fab6d0fb54fe6b5cc
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-07-15 22:00:47 +0700

    feat: Add support for Gallery and per-category RSS feed

--- feed.php
+++ feed.php
@@ -3,8 +3,11 @@
 header('Content-Type: application/json; charset=UTF-8');
 header('Access-Control-Allow-Origin: *');
 
-$url = 'https://student-activity.binus.ac.id/himti/feed';
+$base_url = 'https://student-activity.binus.ac.id/himti';
 
-$feed = implode(file($url));
+if ($_GET['postType'] == 'gallery') $base_url .= '/gallery';
+else if (strlen($_GET['category']) > 0) $base_url .= '/category/' . $category;
+
+$feed = implode(file($base_url . '/feed'));
 $xml = simplexml_load_string($feed);
 echo json_encode($xml);
