3107b9b0aebecbcf424700e7aa67aa153bc59b45
Reinhart Previano Koentjoro <reinhart_previano@yahoo.com>
2021-07-31 23:14:45 +0700

    feat: Use Bootstrap icons

--- components/head.php
+++ components/head.php
@@ -12,6 +12,9 @@
     <link href="/assets/css/tailwind.css" rel="stylesheet">
 <?php endif;?>
 
+<!-- Bootstrap Icons -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
 <!-- Apply custom stylesheet -->
 <link rel="stylesheet" href="style.css">
 
