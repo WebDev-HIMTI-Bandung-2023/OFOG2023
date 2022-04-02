6f9087a51ba91dd7f2cf21c38040849e351c3821
NatanaelGeraldoS <geradonatanael84@gmail.com>
2022-04-02 21:18:53 +0700

    CHANGE: Fix the Vanilla.js Path

--- index.php
+++ index.php
@@ -744,7 +744,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
     </script>
-    <script src="assets/JS/vanilla-tilt.min.js"></script>
+    <script src="assets/js/vanilla-tilt.min.js"></script>
     <script>
     VanillaTilt.init(document.querySelectorAll(".upcomingeventrow"), {
         max: 25,
