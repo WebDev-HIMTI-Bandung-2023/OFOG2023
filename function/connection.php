9eecd32504121070593b1c9aaf8f3971a5a2bc84
NatanaelGeraldoS <geradonatanael84@gmail.com>
2022-08-09 20:38:14 +0700

    CHANGE : Connection

--- function/connection.php
+++ function/connection.php
@@ -1,9 +1,8 @@
 <?php
 
-$server = "localhost";
-$username = "himtiorg_himtijkt";
-$password = "OFOG_2022";
+$username = "root";
+$password = "";
 $database = "himtiorg_himtijkt";
-$port = 3306;
+$port = '3306';
 
 $koneksi = mysqli_connect($server, $username, $password, $database, $port) or die(mysqli_connect_error());
